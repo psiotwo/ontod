fb06381d2e5fb11ef12746354159aeab04d1fccf
psiotwo <Petr.Kremen@labvantage-biomax.com>
2023-03-29 17:43:17 +0200

    Added jitpack.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("maven-publish")
 }
 
 group = "com.github.psiotwo"
@@ -7,6 +8,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 tasks.withType<Test> {
@@ -16,8 +18,17 @@ tasks.withType<Test> {
 dependencies {
     implementation("net.sourceforge.owlapi:owlapi-distribution:5.5.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.github.psiotwo"
+            artifactId = "ontod"
+            version = "0.0.1-SNAPSHOT"
+            from(components["java"])
+        }
+    }
 }
