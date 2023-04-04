a18fe62723147a7c4a6f1f9867ed49621eb80bc1
psiotwo <Petr.Kremen@labvantage-biomax.com>
2023-04-04 15:15:05 +0200

    Improved documentation

--- src/main/kotlin/com/github/psiotwo/ontod/GenerateOBOUpdateScript.kt
+++ src/main/kotlin/com/github/psiotwo/ontod/GenerateOBOUpdateScript.kt
@@ -9,6 +9,8 @@ import java.io.File
 class GenerateOBOUpdateScript {
     /**
      * Generates an OWLOntology in the form of an OBO update script
+     *
+     * @see generate
      */
     fun generate(original: File, update: File, ontologyIri: String, outputFile: File) {
         val originalO = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(original)
@@ -19,7 +21,20 @@ class GenerateOBOUpdateScript {
 
     /**
     }
-     * Generates an OWLOntology in the form of an OBO update script
+     * Generates an OWLOntology in the form of a change ontology (C), simulating how to go from original (O) to update (U) in the following manner:
+     * - C contains all axioms from U that are not in O
+     * - C contains all axioms from O that are not in U, but marked as deprecated
+     *
+     * Then, going from O to U could be simulated as deleting deprecated axioms from union(O,C)
+     *
+     * This is useful for example when resolving unsatisfiable classes in debug files of ROBOT OBO tool. One can take a debug file,
+     * fix unsatisfiable classes and use this tool to generate the fixes needed to be applied to the original ontology.
+     *
+     * @param original - an baseline/original ontology
+     * @param update - an ontology after some (manual) curation
+     * @param ontologyIri - IRI of the resulting ontology
+     *
+     * @return a change ontology as described above.
      */
     fun generate(original: OWLOntology, update: OWLOntology, ontologyIri: IRI): OWLOntology {
         val f = OWLManager.getOWLDataFactory()
